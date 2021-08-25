81ad7dbf734509b1c3571050e274282346cb8bc6
Scott Coons <scoons@cnm.edu>
2021-08-25 13:04:55 -0600

    last-minute tweaks

--- public_html/script.js
+++ public_html/script.js
@@ -1,60 +1,51 @@
 
-    function setupTabs () {
-        document.querySelectorAll(".tabs__button").forEach(button => {
-            button.addEventListener("click", () => {
-                const menu = button.parentElement;
-
-                const tabsContainer = menu.parentElement;
-
-                const tabNumber = button.dataset.forTab;
-
-                // const tabToActivate = document.querySelector(`.tabs__content[data-tab=“${tabNumber}”]`)
-                //const tabToActivate = document.querySelector(`.tabs__content[data-tab=${tabNumber}]`)
-
-                let tabToActivate;
-
-                if(tabNumber === "1") {
-                    tabToActivate = document.getElementById("homeTab");
-                } else if (tabNumber === "2") {
-                    tabToActivate = document.getElementById("portfolioTab");
-                } else if (tabNumber === "3") {
-                    tabToActivate = document.getElementById("formTab");
-                }
-
-                // const tabToActivate = tabsContainer.querySelector(`.tabs__content[data-tab="${tabNumber}"]`);
-
-                // const menuTabs = Array.from(tabsContainer.querySelectorAll(".tabs__content"))
-                // const menuTabs = tabsContainer.querySelectorAll(".tabs__content")
-                // console.log("menuTabs:" +menuTabs)
-                //
-                // let tabToActivate;
-                // for(let i = 0; i<menuTabs.length; i++){
-                //     console.log(i + "tab:" +menuTabs[i])
-                //     if ((i+1).toString() === tabNumber){
-                //         tabToActivate = menuTabs[i]
-                //     }
-                // }
-
-
-
-                menu.querySelectorAll(".tabs__button").forEach(button => {
-                    button.classList.remove("tabs__button--active");
-                });
-
-                document.querySelectorAll(".tabs__content").forEach(tab => {
-                    tab.classList.remove("tabs__content--active");
-                });
-
-                button.classList.add("tabs__button--active");
-                tabToActivate.classList.add("tabs__content--active");
-            });
-        });
-    }
-
-    document.addEventListener("DOMContentLoaded", () => {
-            setupTabs();
-
-            document.querySelectorAll(".tabs").forEach(tabsContainer => {
-                document.querySelector(".tabs__menu .tabs__button").click();
-        });
-    });
+$(document).ready(function () {
+    document.getElementById("contact")
+    $("#contact").validate({
+        debug: true,
+        errorClass: "alert alert-danger",
+        ErrorLabelContainer: "#output-area",
+        errorElement: "div",
+        // these are the rules to define what data we want and what we don't want
+        //each of these groups start with the form input NAME attribute
+        rules: {
+            name: {
+                required: true,
+            },
+            email: {
+                email: true,
+                required: true,
+            },
+            message: {
+                required: true,
+                maxlength: 2000,
+            },
+        },
+        messages: {
+            name: {
+                required: "Name is required",
+            },
+            email: {
+                email: "Please provide a valid email.",
+                required: "Email is required",
+            },
+            message: {
+                required: "Message is required",
+                maxlength: "Message must be 2000 characters or less.",
+            },
+        },
+        submitHandler: () => {
+            $("#contact").ajaxSubmit({
+                type: "POST",
+                url: $("#contact").attr("action"),
+                success: (ajaxOutput) => {
+                    $("#output-area").css("display", "")
+                    $("#output-area").html(ajaxOutput)
+                    if ($(".alert-success" >= 1)) {
+                        $("#contact")[0].reset()
+                    }
+                },
+            })
+        },
+    })
+})
---
+++ public_html/tabs-script.js
@@ -0,0 +1,60 @@
+
+function setupTabs () {
+    document.querySelectorAll(".tabs__button").forEach(button => {
+        button.addEventListener("click", () => {
+            const menu = button.parentElement;
+
+            const tabsContainer = menu.parentElement;
+
+            const tabNumber = button.dataset.forTab;
+
+            // const tabToActivate = document.querySelector(`.tabs__content[data-tab=“${tabNumber}”]`)
+            //const tabToActivate = document.querySelector(`.tabs__content[data-tab=${tabNumber}]`)
+
+            let tabToActivate;
+
+            if(tabNumber === "1") {
+                tabToActivate = document.getElementById("homeTab");
+            } else if (tabNumber === "2") {
+                tabToActivate = document.getElementById("portfolioTab");
+            } else if (tabNumber === "3") {
+                tabToActivate = document.getElementById("formTab");
+            }
+
+            // const tabToActivate = tabsContainer.querySelector(`.tabs__content[data-tab="${tabNumber}"]`);
+
+            // const menuTabs = Array.from(tabsContainer.querySelectorAll(".tabs__content"))
+            // const menuTabs = tabsContainer.querySelectorAll(".tabs__content")
+            // console.log("menuTabs:" +menuTabs)
+            //
+            // let tabToActivate;
+            // for(let i = 0; i<menuTabs.length; i++){
+            //     console.log(i + "tab:" +menuTabs[i])
+            //     if ((i+1).toString() === tabNumber){
+            //         tabToActivate = menuTabs[i]
+            //     }
+            // }
+
+
+
+            menu.querySelectorAll(".tabs__button").forEach(button => {
+                button.classList.remove("tabs__button--active");
+            });
+
+            document.querySelectorAll(".tabs__content").forEach(tab => {
+                tab.classList.remove("tabs__content--active");
+            });
+
+            button.classList.add("tabs__button--active");
+            tabToActivate.classList.add("tabs__content--active");
+        });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    setupTabs();
+    console.log('dom loaded')
+    document.querySelectorAll(".tabs").forEach(tabsContainer => {
+        document.querySelector(".tabs__menu .tabs__button").click();
+    });
+});
